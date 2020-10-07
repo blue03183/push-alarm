0d1c52228193cd8cbfc1efff3dd665713545980c
Sunny <hhe330@naver.com>
2020-10-07 16:56:23 +0900

    푸쉬알람 기능

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import axios from 'axios';
+
+const PUSH_REGISTRATION_ENDPOINT = 'http://e7be3ad3862d.ngrok.io/token';
+const MESSAGE_ENPOINT = 'http://e7be3ad3862d.ngrok.io/message';
 
 export default function App() {
+
+  const [state, setState] = useState({
+    notification: null,
+    messageText: ''
+  });
+
+  const registerForPushNotificationsAsync = async () => {
+    console.log('111');
+    const { status } = await Premissions.askAsync(Permissions.NOTIFICATIONS);
+
+    console.log('222');
+    if (status !== 'granted') {
+      return;
+    }
+    let token = await Notifications.getExpoPushTokenAsync();
+
+    return axios.post(PUSH_REGISTRATION_ENDPOINT, {
+      token: {
+        value: token
+      },
+      user: {
+        username: 'sunny',
+        name: 'pushapp'
+      }
+    })
+
+    const notificationSubscription = Notifications.addListener(handleNotification);
+  }
+
+  const handleNotification = (notification) => {
+    setState({ notification });
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
+  const handleChangeText = (text) => {
+    setState({ messageText: text });
+  }
+
+  const sendMessage = async () => {
+    axios.post(MESSAGE_ENPOINT, {
+      message: state.messageText
+    });
+
+    setState({ messageText: '' });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        value={state.messageText}
+        onChangeText={handleNotification}
+        style={styles.textInput}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={sendMessage}>
+        <Text style={styles.buttonText}>Send</Text>
+      </TouchableOpacity>
+      
+      {/* {state.notification ? renderNotification() : null} */}
     </View>
   );
 }
@@ -18,4 +84,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    width: 300,
+    height: 50,
+    backgroundColor: 'silver'
+  },
+  button: {
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+  },
+  buttonText: {
+    color: 'gray'
+  }
 });
